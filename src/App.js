c9221b4aead139b83c0cd9a3d0abb3a89e05d9f8
UshanShakya <usnshakya4@gmail.com>
2022-11-09 00:42:18 +0545

    Day 2 : Creating new expense items and managing states

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 import Expenses from "./components/Expenses/Expenses";
-
+import NewExpense from "./components/NewExpense/NewExpense";
 const App = () => {
+  const addExpenseHandler = (expense) =>{
+    // console.log(expense);
+  }
+
   const expenses = [
     { id: 'e1', title: "New Mobile", amount: 300, date: new Date(2021, 5, 1) },
     { id: 'e2', title: "Food", amount: 200, date: new Date(2021, 5, 1) },
@@ -13,6 +17,7 @@ const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses}/>
     </div>
   );
